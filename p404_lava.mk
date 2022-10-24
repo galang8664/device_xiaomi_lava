4aaff63266d19bada475e5fb2e2e2f7ee1902567
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-10-24 13:41:04 +0600

    Update p404_lava.mk

--- p404_lava.mk
+++ p404_lava.mk
@@ -26,7 +26,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/404/configs/common.mk)
 
 #
 # All components inherited here go to system_ext image
