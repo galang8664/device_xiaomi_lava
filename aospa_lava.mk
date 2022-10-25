ad6e24ced77f415a9f4e0c2ea444609903545967
galang8664 <95033122+galang8664@users.noreply.github.com>
2022-10-25 07:04:22 +0700

    Update and rename p404_lava.mk to aospa_lava.mk

--- aospa_lava.mk
+++ aospa_lava.mk
@@ -41,7 +41,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lava
-PRODUCT_NAME := p404_lava
+PRODUCT_NAME := aospa_lava
 PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := xiaomi
 
