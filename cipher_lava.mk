26d354471d06099f6967ffee1cd8e6c0a6648ecf
galang8664 <95033122+galang8664@users.noreply.github.com>
2022-11-06 16:55:28 +0700

    Update and rename syberia_lava.mk to cipher_lava.mk

--- cipher_lava.mk
+++ cipher_lava.mk
@@ -1,4 +1,4 @@
-#
+cipher#
 # Copyright (C) 2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -27,7 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/syberia/common.mk)
+$(call inherit-product, vendor/cipher/config/common_full_phone.mk)
 
 #
 # All components inherited here go to system_ext image
@@ -37,7 +37,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lava
-PRODUCT_NAME := syberia_lava
+PRODUCT_NAME := cipher_lava
 PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := xiaomi
 
