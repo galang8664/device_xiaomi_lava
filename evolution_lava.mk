8a8bc171d892bf26b098ed5287ee3ad2bc79dbc8
galang8664 <95033122+galang8664@users.noreply.github.com>
2022-11-23 11:32:31 +0700

    Update and rename syberia_lava.mk to evolution_lava.mk

--- evolution_lava.mk
+++ evolution_lava.mk
@@ -27,7 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/syberia/common.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 #
 # All components inherited here go to system_ext image
@@ -37,7 +37,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lava
-PRODUCT_NAME := syberia_lava
+PRODUCT_NAME := evolution_lava
 PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := xiaomi
 
